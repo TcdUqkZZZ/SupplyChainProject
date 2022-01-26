1fe787cd7945492551e480860eefc281331ca5c0
TcdUqkZZZ <taccid.urquan@gmail.com>
2022-01-26 20:42:09 +0100

    fixed webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,13 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     resolve: { modules: ['node_modules'] ,
+                fallback: {
+                    "os": require.resolve("os-browserify/browser"),
+                    "https": require.resolve("https-browserify"),
+                    "http": require.resolve("stream-http"),
+                    "crypto": require.resolve("crypto-browserify"),
+                    "stream": require.resolve("stream-browserify")
+                }
                 },
   mode: 'development',
   entry: "./src/index.js",
